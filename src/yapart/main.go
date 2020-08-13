d1336a2f6f50682fd857c13dd81ce396d4bdecc7
Alekseyya <alekseyyarchuk@gmail.com>
2020-08-14 02:56:23 +0300

    Первый чатик

--- src/yapart/main.go
+++ src/yapart/main.go
@@ -15,18 +15,5 @@ func init() {
 }
 
 func main() {
-	// var b models.IBrand = &models.Brand{}
-	// b.Add()
-	// var m models.IMark = &models.Mark{}
-	// m.Add()
-
-	// var brands []models.Brand
-	// db.Select("Id, name, picture_id").Find(&brands)
-	// for _, v := range brands {
-	// 	fmt.Println(v)
-	// }
-
-	//main
-
-	fmt.Println("asdasd")
+	fmt.Printf("assdfsd")
 }
