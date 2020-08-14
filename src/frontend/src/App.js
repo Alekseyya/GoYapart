105210de2537021241376b97128cb3f0aeffd688
Alekseyya <alekseyyarchuk@gmail.com>
2020-08-14 19:18:08 +0300

    Компонентный подход

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,11 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import { connect, sendMsg } from "./api";
 import Header from "./components/Header/Header.jsx";
+import ChatHistory from "./components/ChatHistory/ChatHistory.jsx"
 
 class App extends Component {
   constructor(props) {
     super(props);
-    connect();
+    this.state = {
+      chatHistory : []
+    }
+  }
+
+  componentWillMount(){
+    connect((msg) => {
+      console.log("New message")
+      this.setState(preState => ({
+        chatHistory : [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state)
+    })
   }
 
   send() {
@@ -19,6 +32,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <ChatHistory chatHistory={this.state.chatHistory} />
         <button onClick={this.send}>Hit</button>
       </div>
     );
